dd2e57a033fb82a2cf52959d5597cb00bbc526d6
Miggy <miguelramirez25@aol.com>
2022-04-27 18:59:44 -0400

    Plugin Update

--- membership/hooks/_success.php
+++
@@ -1,9 +0,0 @@
-<?php if(count(get_included_files()) ==1) die(); //Direct Access Not Permitted?>
-<br /> <br />
-
-<div class="card mb-4 py-3 border-bottom-success text-center">
-    <div class="card-body">
-         Payment was successful! Enjoy <?=$settings->site_name?>!<br /><Br /> The receipt was send to <?=$userdetails->email?>
-    </div>
-</div></div></div>
-</div>
